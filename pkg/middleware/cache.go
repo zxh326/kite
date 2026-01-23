07c79866d3b0e0d1db95d5e066f9d9c82c0751c8
Zzde <zhangxh1997@gmail.com>
2026-01-23 13:15:46 +0800

    fixed
    Signed-off-by: Zzde <zhangxh1997@gmail.com>

--- pkg/middleware/cache.go
+++ pkg/middleware/cache.go
@@ -1,39 +1,13 @@
 package middleware
 
 import (
-	"strings"
-
 	"github.com/gin-gonic/gin"
 )
-
-// staticExtensions defines the file extensions for static assets that should be cached
-var staticExtensions = []string{
-	".js", ".css", ".png", ".jpg", ".jpeg", ".gif", ".svg", ".ico",
-	".woff", ".woff2", ".ttf", ".eot",
-}
 
 // StaticCache adds cache-control headers for static assets
 func StaticCache() gin.HandlerFunc {
 	return func(c *gin.Context) {
-		path := c.Request.URL.Path
-
-		// Check if the request is for a static asset
-		shouldCache := strings.HasPrefix(path, "/assets/")
-		if !shouldCache {
-			for _, ext := range staticExtensions {
-				if strings.HasSuffix(path, ext) {
-					shouldCache = true
-					break
-				}
-			}
-		}
-
-		if shouldCache {
-			// Cache static assets for 1 year (31536000 seconds)
-			// immutable means the resource won't change at this URL
-			c.Header("Cache-Control", "public, max-age=31536000, immutable")
-		}
-
+		c.Header("Cache-Control", "public, max-age=31536000, immutable")
 		c.Next()
 	}
 }
